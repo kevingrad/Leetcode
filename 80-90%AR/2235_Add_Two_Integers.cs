7bd697a7a5c52054c32a0565d6791c6a64350c3b
kevingrad <70232994+kevingrad@users.noreply.github.com>
2022-12-24 20:39:23 -1000

    Create 2235_Add_Two_Integers.cs

---
+++ 80-90%AR/2235_Add_Two_Integers.cs
@@ -0,0 +1,9 @@
+/*
+Given two integers num1 and num2, return the sum of the two integers.
+*/
+
+public class Solution {
+    public int Sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
